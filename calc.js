1c4d837253d82414767572790a94b12cfd0cfdd0
Vinayak Raj Mishra <63664741+vinayakrajmishra@users.noreply.github.com>
2023-08-24 16:03:14 +0530

    Update calc.js
    This commit to fix the issue I created about eval() function.

--- calc.js
+++ calc.js
@@ -1,19 +1,17 @@
 // Get the result area element
 var resultArea = document.getElementById('resultarea');
 
-
 // Get all calculator buttons
 var buttons = document.getElementsByClassName('ele');
 
 // Initialize the calculator input
 var input = '';
 
-
 // Add click event listener to each button
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function() {
     var buttonValue = this.innerHTML;
-    
+
     // Handle button clicks
     if (buttonValue === 'C') {
       // Clear the input
@@ -24,18 +22,39 @@ for (var i = 0; i < buttons.length; i++) {
     } else if (buttonValue === '=') {
       // Evaluate the input expression
       try {
-        input = eval(input);
+        input = evaluateExpression(input);
       } catch (error) {
         // Handle any errors in the expression
         input = 'Error';
       }
     } else {
       // Append the button value to the input
-      input += buttonValue;
+      input = handleSpecialCases(input, buttonValue);
     }
-    
+
     // Update the result area with the current input value
     resultArea.textContent = input;
-    
+
   });
+}
+
+function handleSpecialCases(currentInput, buttonValue) {
+  // Handle special cases
+  if (/[\+\*\/-]$/.test(currentInput) && /[\+\*\/-]/.test(buttonValue)) {
+    // Replace the last operator with the new one
+    return currentInput.slice(0, -1) + buttonValue;
+  } else if (currentInput.endsWith('//') || currentInput.endsWith('**')) {
+    // Replace '//' with '/' and '**' with '*'
+    return currentInput.slice(0, -1) + buttonValue;
+  }
+
+  return currentInput + buttonValue;
+}
+
+function evaluateExpression(expression) {
+  // Replace '//' with '/' and '**' with '*'
+  expression = expression.replace('//', '/');
+  expression = expression.replace('**', '*');
+
+  return eval(expression);
 }
